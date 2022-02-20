693b36c76f7cd4494ad3d44ce3fd0a517bb02e7c
rl27 <rl27@princeton.edu>
2022-02-20 18:55:45 -0500

    Mag to ori fix

--- source/data_glob_speed.py
+++ source/data_glob_speed.py
@@ -79,7 +79,9 @@ class GlobSpeedSequence(CompiledSequence):
         #self.features = np.concatenate([glob_gyro, glob_acce], axis=1)[start_frame:]
         self.features = np.concatenate([magn], axis=1)[start_frame:]
         #self.targets = glob_v[start_frame:, :2]
-        self.targets = quaternion.as_float_array(ori_q)[start_frame:]
+        y = quaternion.as_float_array(ori_q)[start_frame:]
+        y = (y[w:] - y[:-w]) / dt
+        self.targets = y
         self.orientations = quaternion.as_float_array(ori_q)[start_frame:]
         self.gt_pos = tango_pos[start_frame:]
 
