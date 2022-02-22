39ffdf495af3700a49372c2bea410de3d6a76dd6
rl27 <rl27@princeton.edu>
2022-02-22 16:08:34 -0500

    Fix list utils

--- source/list_utils.py
+++ source/list_utils.py
@@ -5,20 +5,19 @@ import numpy as np
 def gen_list(dirname):
     testlist = ""
     for item in os.listdir(dirname):
-    testlist += item + '\n'
+        testlist += item + '\n'
     f = open("my_list.txt", "w")
     #f.write("a047_2")
     f.write(testlist)
     f.close()
 
-
 # Generate train list, given dataset directory
 def gen_train_list(dirname):
     items = ""
     for item in os.listdir(dirname + "train_dataset_1"):
-    items += "train_dataset_1/" + item + '\n'
+        items += "train_dataset_1/" + item + '\n'
     for item in os.listdir(dirname + "train_dataset_2"):
-    items += "train_dataset_2/" + item + '\n'
+        items += "train_dataset_2/" + item + '\n'
     f = open("train_list.txt", "w")
     f.write(items)
     f.close()
@@ -27,20 +26,20 @@ def gen_train_list(dirname):
 def gen_train_val(dirname):
     items = []
     for item in os.listdir(dirname + "train_dataset_1"):
-    items.append("train_dataset_1/" + item + '\n')
+        items.append("train_dataset_1/" + item + '\n')
     for item in os.listdir(dirname + "train_dataset_2"):
-    items.append("train_dataset_2/" + item + '\n')
+        items.append("train_dataset_2/" + item + '\n')
     train = np.random.choice(items, 70, replace=False)  # 70 out of 85 is train
     val = []
     for i in items:
-    if i not in train:
-      val.append(i)  # 15 out of 85 is val
+        if i not in train:
+            val.append(i)  # 15 out of 85 is val
     train_str = ""
     for i in train:
-    train_str += i
+        train_str += i
     val_str = ""
     for i in val:
-    val_str += i
+        val_str += i
     f = open("train_list.txt", "w")
     f.write(train_str)
     f.close()
