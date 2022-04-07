619f9638cec3b335cd2933fb448924fac6b80b33
rl27 <rl27@princeton.edu>
2022-04-07 18:41:25 -0400

    test

--- source/ronin_vit.py
+++ source/ronin_vit.py
@@ -32,10 +32,10 @@ def get_model(args, **kwargs):
         image_size = -1,
         patch_size = 20,
         num_classes = 2,
-        dim = 50,
+        dim = 200,
         depth = 6,
-        heads = 16,
-        mlp_dim = 1024,
+        heads = 8,
+        mlp_dim = 2048,
         dropout = 0.2, # linear dropout
         emb_dropout = 0.2 # conv dropout
     )
